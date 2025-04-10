f58c723ab8395a83942b66e296cb0e1506f59bd2
victoria <reneva_v@mail.ru>
2025-04-10 09:52:30 +0300

    smth fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,8 +32,8 @@ tasks.jar {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.liquibase:liquibase-core")
-    runtimeOnly("org.postgresql:postgresql")
+    implementation("org.liquibase:liquibase-core:4.25.1")  // Явное указание версии
+    implementation("org.postgresql:postgresql:42.6.0")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
